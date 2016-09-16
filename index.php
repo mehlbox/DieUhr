02f16670a53b60cea6a46bb2103e1c027d134814
mehlbox <root@localhost.localdomain>
2016-09-16 19:22:37 +0200

    style

--- index.php
+++ index.php
@@ -28,23 +28,30 @@ header( "refresh:120;url=/" );
 <head>
 <script type="text/javascript" src="js/clock.js"></script>
 <script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript">
+function formSubmit() {
+    document.getElementById("test").submit();
+}
+</script>
 <style>
 body {
 	background-color: #FFFFFF;
 	font-size: 5vw;
 }
 div {
-	background-color: #DDDDDD;
+	background-color: #EAEAEA;
 	padding: 1vw
 	margin: 1vw;
+	-webkit-border-radius: 2vw;
 }
-div div{
-	background-color: #000000;
-}
-input {
+input, select {
 	font-size: 5vw;
 	padding: 2vw;
 	margin: 1vw;
+	border: solid 0.1vw #000000;
+	-webkit-border-radius: 2vw;
+	-webkit-box-shadow: #555555 0.5vw 0.5vw 0.5vw;
+	background-image: -webkit-linear-gradient(top, #D4D5D5, #fdfdfd);
 }
 .box, .boxw{
 	float: left;
@@ -54,7 +61,6 @@ input {
 	-webkit-border-radius: 3vw;
 	-webkit-box-shadow: #B3B3B3 1vw 1vw 1vw;
 	box-sizing: border-box;
-	
 }
 .box {
 	width: 46vw;
@@ -71,6 +77,7 @@ input {
 	text-align:center;
 	display:block;
 	font-size: 21vw;
+	-webkit-border-radius: 0;
 }
 .the_clock {
 	font-size:100%;
@@ -88,6 +95,8 @@ input {
 	overflow: hidden;
 	white-space: nowrap;
 	-webkit-animation: marquee 15s linear infinite;
+	background-color: #000000;
+	-webkit-border-radius: 0;
 }
 .simple {
 	font-size: 25%;
@@ -96,21 +105,23 @@ input {
 	position: relative;
 	overflow: hidden;
 	white-space: pre;
+	background-color: #000000;
+	-webkit-border-radius: 0;
 }
 textarea {
 	width: 95%;
-	font-size: 5vmin;
+	font-size: 5vw;
 	background-color:#FFFF66; 
-	border: 2pt solid #000000;
-	padding: 1vmin;
-	margin: 1vmin;
+	border: 0.3vw solid #000000;
+	padding: 1vw;
+	margin: 1vw;
 }
 </style>
 </head>
 <body onload="startClock(true)">
 <form method="post" >
 <?php if (file_get_contents($mode) != 'wedding') { ?>
-	<div class="boxw">Ansicht:
+	<div class="boxw"><?php if (file_get_contents($preview)) { echo 'Vorschau:'; } else { echo 'Live Ansicht:'; } ?>
 		<div id="clock">
 		<span class="the_clock">
 			<span class='cl_hours'></span><span class='cl_minutes'></span>
@@ -129,14 +140,11 @@ if (!file_get_contents($status) && !file_get_contents($preview)) {
 }
 ?>
 		</div>
-		</br>
 <?php
 if (file_get_contents($preview)) {
-        echo '
-		<div style="background-color: #99DD55; border: 0; padding: 1vmin;"><input type="submit" name="preview_off" value="umschalten" />Vorschau</div>';
+        echo '<input type="submit" name="preview_off" value="zur Live Ansicht wechseln" style="width: 87vw;" />';
 } else {
-        echo '
-		<div style="background-color: #FF7755; border: 0; padding: 1vmin;"><input type="submit" name="preview_on"  value="umschalten" />Live Anzeige</div>';
+        echo '<input type="submit" name="preview_on"  value="zur Vorschau wechseln"     style="width: 87vw;" />';
 } ?>	
 	</div>	
 <?php } ?>
@@ -149,7 +157,7 @@ if (file_get_contents($preview)) {
 			<input type="submit" name="reset" value="Das Lied ..." />
 	</div>
 <?php } else { ?>
-	<div class="boxw">Ansicht nicht verfügbar.
+	<div class="boxw">Vorschau nicht verfügbar.
 	</div>
 	<div class="boxw">Namen Brautpaar:
 			<textarea name="message"><?php echo $formtext; ?></textarea>
@@ -158,27 +166,24 @@ if (file_get_contents($preview)) {
 	</div>
 <?php } ?>
 
-	<div class="box">Modus:
-		<div style="background-color: #999999; border: 0; padding: 1vmin;">
-			<select name="mode" style="font-size: 5vw; border: 0; padding: 1vmin; margin: 2%; width: 95%;">
+	<div class="box">Anzeigemodus:
+			<select name="mode" style="width: 39vw;" onchange="if(this.value != 0) this.form.submit();">
 				<option value="marquee"        <?php if (file_get_contents($mode) == 'marquee')        echo 'selected="selected"'; ?> >Laufschrift</option>
 				<option value="textblock"      <?php if (file_get_contents($mode) == 'textblock')      echo 'selected="selected"'; ?> >Textblock</option>
 				<option value="wedding"        <?php if (file_get_contents($mode) == 'wedding')        echo 'selected="selected"'; ?> >Hochzeit</option>
 				<!--<option value="largetextblock" <?php if (file_get_contents($mode) == 'largetextblock') echo 'selected="selected"'; ?> >Text + keine Uhr</option>-->
 			</select>
-			<input type="submit" value="OK" />
-		</div>
 	</div>
 	<div class="box">Status:
 <?php
 if (file_get_contents($status)) {
         echo '
-		<div style="background-color: #99FF55; border: 0; padding: 1vmin; ">EIN</br>
-		<input type="submit" name="status_off" value="ausschalten" /></div>';
+		<div style="background-color: #99DD55; padding: 1vw; text-align:center;">EIN</br>
+		<input type="submit" name="status_off" value="ausschalten" style="width: 37vw;" /></div>';
 } else {
         echo '
-		<div style="background-color: #FF9955; border: 0; padding: 1vmin; ">AUS</br>
-		<input type="submit" name="status_on"  value="einschalten" /></div>';
+		<div style="background-color: #FF7755; padding: 1vw; text-align:center;">AUS</br>
+		<input type="submit" name="status_on"  value="einschalten" style="width: 37vw;" /></div>';
 } ?>
 	</div>
 </form>
