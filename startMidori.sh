b2b90186b716951d75e667faa239f500a39d9606
mehlbox <mehlbox@github.com>
2016-09-16 20:07:05 +0200

    fix address

--- startMidori.sh
+++ startMidori.sh
@@ -6,6 +6,6 @@ xset s noblank # don't blank the video device
 unclutter &
 matchbox-window-manager &
 #while true; do # Use this to restart midori when crashed
-midori -e Fullscreen -a /home/pi/uhr/index.html
+midori -e Fullscreen -a http://localhost/display.php
 #done
 
