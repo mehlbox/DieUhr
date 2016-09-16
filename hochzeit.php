ef9daa2aa9da698c6a5bbaf6ad7ae63efb33c24e
mehlbox <mehlbox@github.com>
2016-09-16 16:28:08 +0200

    add refresh

--- hochzeit.php
+++ hochzeit.php
@@ -3,7 +3,11 @@ $file = $_SERVER['DOCUMENT_ROOT'].'/data/wedding.txt';
 $status = $_SERVER['DOCUMENT_ROOT'].'/data/status.inf' ;
 $mode = $_SERVER['DOCUMENT_ROOT'].'/data/mode.inf';
 $formtext = file_get_contents($file);
-if ( !file_get_contents($status) || file_get_contents($mode) != 'wedding') header("Location: display.php");
+if ( !file_get_contents($status) || file_get_contents($mode) != 'wedding') { 
+	header("Location: display.php");
+} else {
+	header( "refresh:600;url=hochzeit.php" );
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
