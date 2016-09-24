b2a021f0c69443822e3da7029daa9811bd4d5d3a
mehlbox <mehlbox@github.com>
2016-09-24 23:26:21 +0200

    add load save

--- js/function.js
+++ js/function.js
@@ -62,3 +62,25 @@ function chkOnOff(){
 		$('#switch').css("background-color", "#FF7755");
 	}
 }
+
+function setCookie(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (30*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
