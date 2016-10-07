8983f388b3ed26cdbfc236656edaaa7e9035f7a0
mehlbox <mehlbox@github.com>
2016-10-07 15:19:03 +0200

    add option

--- startMidori.sh
+++ startMidori.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 export DISPLAY=":0"
-xset -dpms # disable DPMS (Energy Star) features.
-xset s off # disable screen saver
-xset s noblank # don't blank the video device
-unclutter &
-matchbox-window-manager &
-#while true; do # Use this to restart midori when crashed
+xset -dpms 		# disable DPMS (Energy Star) features.
+xset s off 		# disable screen saver
+xset s noblank 	# don't blank the video device
+matchbox-window-manager -use_cursor no&
+#while true; do # Use this to automatically restart midori when crashed
 midori -e Fullscreen -a http://localhost/display.html
 #done
 
