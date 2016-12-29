acdc156a99d5a9a8ff07e99d5e08a9303fb047c9
mehlbox <mehlbox@github.com>
2016-12-29 16:53:16 +0100

    clean up

--- startMidori.sh
+++ startMidori.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-export DISPLAY=":0"
-xset -dpms 		# disable DPMS (Energy Star) features.
-xset s off 		# disable screen saver
-xset s noblank 	# don't blank the video device
+export DISPLAY=:0
+/usr/bin/xset -dpms 		# disable DPMS (Energy Star) features.
+/usr/bin/xset s off 		# disable screen saver
+/usr/bin/xset s noblank 	# don't blank the video device
 matchbox-window-manager -use_cursor no&
-#while true; do # Use this to automatically restart midori when crashed
 midori -e Fullscreen -a http://localhost/display.html
-#done
-
