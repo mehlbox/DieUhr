7d524e60658d64d73a1a1137b93d225ca40b7914
mehlbox <leonid.firus@gmail.com>
2017-05-22 10:02:16 +0200

    fix network issue

--- startMidori.sh
+++ startMidori.sh
@@ -3,5 +3,6 @@ export DISPLAY=:0
 /usr/bin/xset -dpms 		# disable DPMS (Energy Star) features.
 /usr/bin/xset s off 		# disable screen saver
 /usr/bin/xset s noblank 	# don't blank the video device
+sleep 5						# prevent network issues
 matchbox-window-manager -use_cursor no&
 midori -e Fullscreen -a http://localhost/display.html
